ff6b4d8975a51c1479f03c2bc45c67c3faffecc9
Dakota Marshall <me@dakotamarshall.net>
2025-07-18 18:38:15 -0400

    Convert flake to build from source

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "LegendsViewer-Next";
 
   inputs = {
-
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
@@ -12,68 +11,123 @@
     systems.url = "github:nix-systems/default-linux";
   };
 
-  outputs = { flake-parts, systems, nixpkgs, ... }@inputs:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-
+  outputs = {
+    flake-parts,
+    systems,
+    nixpkgs,
+    self,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
       systems = import systems;
 
-      perSystem = { config, pkgs, system, ... }:
-        let
-          inherit (pkgs) stdenv;
-          nativeBuildInputs = with pkgs; [
-            zlib
-            makeWrapper
-            icu
-            glib
-            fontconfig
-            autoPatchelfHook
+      perSystem = {
+        config,
+        pkgs,
+        system,
+        lib,
+        ...
+      }: let
+        inherit (pkgs) stdenv;
+        nativeBuildInputs = with pkgs; [
+          dotnetCorePackages.sdk_8_0
+          dotnetCorePackages.aspnetcore_8_0
+          copyDesktopItems
+          (writeShellScriptBin "npm" "")
+        ];
+        src = ./.;
+
+        # # Setup Release Info
+        repoOwner = "Kromtec";
+        repoName = "LegendsViewer-Next";
+        semVer = "1.0.0";
+        version = "${semVer}-source";
+        frontend = pkgs.callPackage ./frontend.nix {
+          inherit src self version;
+        };
+      in {
+        formatter = pkgs.nixfmt-rfc-style;
+        packages.default = pkgs.buildDotnetModule rec {
+          inherit src version nativeBuildInputs;
+          pname = "legendsviewer-next";
+
+          dotnet-sdk = pkgs.dotnetCorePackages.sdk_8_0;
+          dotnet-runtime = pkgs.dotnetCorePackages.aspnetcore_8_0;
+          nugetDeps = ./deps.json;
+
+          projectFile = "./LegendsViewer.Backend/LegendsViewer.Backend.csproj";
+          testProjectFile = "./LegendsViewer.Backend.Tests/LegendsViewer.Backend.Tests.csproj";
+          executables = [meta.mainProgram];
+          desktopItems = [
+            (pkgs.makeDesktopItem {
+              name = pname;
+              desktopName = "Legends Viewer";
+              genericName = "DF Legends Export Browser";
+              comment = meta.description;
+              icon = "${frontend}/dist/ceretelina.png";
+              tryExec = meta.mainProgram;
+              exec = meta.mainProgram;
+
+              # Remove when https://github.com/Kromtec/LegendsViewer-Next/pull/55 gets
+              # released
+              terminal = true;
+
+              categories = [
+                "Game"
+                "RolePlaying"
+                "Simulation"
+              ];
+              keywords = [
+                "df"
+                "dwarf"
+                "fortress"
+                "legends"
+                "viewer"
+              ];
+            })
           ];
 
-          # # Setup Release Info
-          # repoOwner = "Kromtec";
-          # repoName = "LegendsViewer-Next";
-          # # Grab latest release from Github API
-          # latestReleaseJson = builtins.fetchurl {
-          #   url = "https://api.github.com/repos/${repoOwner}/${repoName}/releases/latest";
-          #   sha256 = lib.fakeHash;
-          # };
-          # releaseData = builtins.fromJSON latestReleaseJson;
-          # releaseVersion = lib.strings.removePrefix "v" releaseData.tag_name; # Strip v out of version
-        in {
+          installPhase = ''
+            runHook preInstall
 
-          formatter = pkgs.nixfmt-rfc-style;
-          packages.default = stdenv.mkDerivation rec {
-            inherit nativeBuildInputs;
-            pname = "LegendsViewer-Next";
-            version = "1.2.0";
+            lib=$out/lib/${pname}
 
-            src = pkgs.fetchzip {
-              url =
-                "https://github.com/Kromtec/LegendsViewer-Next/releases/download/v${version}/LegendsViewer-${version}-linux-x64.zip";
-              hash = "sha256:0gry94v683jjwl4wa2nk0p5dj5sk30hha2p6pi5ckw9mhwmh20gm";
-              stripRoot = false;
-            };
+            mkdir -p $lib
 
-            # This is probably a horrendous way of doing this, open to better methods
-            installPhase = ''
-              runHook preInstall
-              mkdir -p $out/share/legendsviewer/
-              mkdir -p $out/bin
-              mv * $out/share/legendsviewer
-              ln -s  $out/share/legendsviewer/LegendsViewer $out/bin/LegendsViewer
-              runHook postInstall
-            '';
-            # Even with icu installed dotnet still whines, so I turned this off
-            postInstall = ''
-              wrapProgram "$out/bin/LegendsViewer" --set 'DOTNET_SYSTEM_GLOBALIZATION_INVARIANT' '1'
-            '';
+            cp ./LegendsViewer.Backend/bin/Release/*/*/* $lib
+            ln -s ${frontend} $lib/${frontend.pname}
 
-            meta = {
-              homepage = "https://github.com/Kromtec/LegendsViewer-Next";
-              description =
-                "Recreates Dwarf Fortress' Legends Mode from exported files. ";
-            };
+            runHook postInstall
+          '';
+
+          doInstallCheck = true;
+          nativeInstallCheckInputs = [
+            pkgs.curl
+          ];
+
+          installCheckPhase = ''
+            runHook preInstallCheck
+
+            timeout 10 $out/bin/LegendsViewer &
+            sleep 5
+
+            # Static server is up
+            curl -f http://localhost:8081 | grep "<!doctype html>"
+
+            # # Version matches expected
+            # curl -f http://localhost:5054/api/version | grep version
+
+            runHook postInstallCheck
+          '';
+
+          meta = {
+            homepage = "https://github.com/${repoOwner}/${repoName}";
+            description = "Recreates Dwarf Fortress' Legends Mode from exported files. ";
+            license = lib.licenses.mit;
+            mainProgram = "LegendsViewer";
+            platforms = with lib.platforms; lib.intersectLists (darwin ++ linux) (x86_64 ++ aarch64);
           };
         };
+      };
     };
 }
